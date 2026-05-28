e2c09ae025ef38a1593cc463e756d874eb6c7ad3
LamineJS <mamadou-lamine.diallo@laplateforme.io>
2026-05-28 10:48:01 +0200

    chore: initialize Spring Boot project with core dependencies

---
+++ src/main/java/com/pos_mvp/PosMvpApplication.java
@@ -0,0 +1,13 @@
+package com.pos_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pos_mvp/PosMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
