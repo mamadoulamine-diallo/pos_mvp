a5084471ca90c84877fe15f200fa9fae05b31635
LamineJS <mamadou.lamine-diallo@laplateforme.io>
2026-09-09 20:27:39 +0200

    feat(v2): enable Feign clients in sale service

--- microservices/sale-service/src/main/java/com/projectpos/saleservice/SaleServiceApplication.java
+++ microservices/sale-service/src/main/java/com/projectpos/saleservice/SaleServiceApplication.java
@@ -2,12 +2,16 @@ package com.projectpos.saleservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SaleServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SaleServiceApplication.class, args);
+        SpringApplication.run(
+                SaleServiceApplication.class,
+                args
+        );
     }
-
 }
