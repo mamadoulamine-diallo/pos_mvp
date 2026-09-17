b2417fd6523be79789d999f3d3dfd69cdfeee04a
LamineJS <mamadou.lamine-diallo@laplateforme.io>
2026-09-17 09:47:25 +0200

    feat(v2): initialize activity service

---
+++ microservices/activity-service/src/main/java/com/projectpos/activityservice/ActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projectpos.activityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityServiceApplication.class, args);
+    }
+
+}
